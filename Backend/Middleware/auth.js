8b777b2cc9fbb3dadac146ff9208ff11d1eff7d1
Asif <asifpatel068@gmail.com>
2024-01-06 13:05:49 +0530

    implimented Authorization to protect routes

---
+++ Backend/Middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt=require("jsonwebtoken");
+require('dotenv').config()
+
+const auth=async(req,res,next)=>{
+    const token=req.headers.authorization;
+
+    if(!token){
+        return res.status(401).send({error:"Unauthorized, Please Login"})
+    }
+    try{
+        const decoded=jwt.verify(token,process.env.KEY);
+        if(decoded){
+             req.user={userId:decoded.userId,userType:decoded.userType};
+             console.log(req.user)
+            next()
+        }
+       
+    }catch(err){
+        return res.status(401).send({error:"Unauthorized error, Please Login"})
+    }
+}
+
+module.exports={
+    auth
+}
